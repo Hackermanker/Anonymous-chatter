16fb112e862d5d8079ef283a2de9137fa7568d97
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2022-09-01 17:33:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from pyrogram import Client, filters
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+ROGUE = Client(
+    name="RogueBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("Bot Started")
+
+ROGUE.run()
+
+
+
+    
+    
 
