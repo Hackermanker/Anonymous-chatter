ad5b34f5347178183c50e5c14195be28c1cec908
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2022-10-16 21:08:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ START_BUTTONS = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('Updates', url=f"https://telegram.me/botupdatesastra"), 
         InlineKeyboardButton('Deploy your own', url=f"https://GitHub.com/Hackermanker/Anonymous-chatter"),
-        ],[
+        ]]
+    )
 @Bot.on_message(filters.private & filters.all & filters.user(USER_ID))
 async def start(bot, update):
     await bot.send_message(CHAT_ID,update.text)
