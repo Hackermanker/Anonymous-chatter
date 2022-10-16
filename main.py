a6259bf9660bfa7b983029aeeb9914ab1049e8d4
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2022-10-16 22:14:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,7 @@ Bot = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
-
-START_TEXT = """ `Hai {}, 
-Am a Anonymous messagin bot Bot You can use me to chat as an anonymous admin . 
-Use /help For knowing more.`
-"""
-START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Updates', url=f"https://telegram.me/botupdatesastra"), 
-        InlineKeyboardButton('Deploy your own', url=f"https://GitHub.com/Hackermanker/Anonymous-chatter"),
-        ]]
-    )
+    
 @Bot.on_message(filters.private & filters.all & filters.user(USER_ID))
 async def start(bot, update):
     await bot.send_message(CHAT_ID,update.text)
